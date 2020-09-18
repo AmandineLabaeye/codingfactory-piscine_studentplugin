9a354183cbca28899ee470c75bf9dca5f7862129
AmandineLabaeye <43751498+AmandineLabaeye@users.noreply.github.com>
2020-09-18 15:51:14 +0200

    Commentaire + Niveau
    Niveau sans batterie, Niveau de code 10 + 16 track, réussi,
    Niveau avec batterie, Niveau de code 10 + 8 track réussi / 16

--- src/main/java/fr/rphstudio/codingdojo/students/Student23.java
+++ src/main/java/fr/rphstudio/codingdojo/students/Student23.java
@@ -20,8 +20,10 @@ public class Student23 extends PodPlugIn {
     //-------------------------------------------------------
     // DECLARE YOUR OWN VARIABLES AND FUNCTIONS HERE
 
+    // Variable Global la charge
     boolean charge = true;
 
+    // Remplace la fonction distanceBetween
     float distanceBetween(float x0, float y0, float x1, float y1) {
         float dX = x1 - x0;
         float dY = y1 - y0;
@@ -29,6 +31,7 @@ public class Student23 extends PodPlugIn {
         return sqrt((dX * dX) + (dY * dY));
     }
 
+    // Remplace la fonction getFirstCharging
     int getFirstCharging() {
 
         int nbcheckpointmax = getNbRaceCheckPoints(); // Récupération du nombre maximum du checkpoint
@@ -41,23 +44,35 @@ public class Student23 extends PodPlugIn {
         return checkcharge;
     }
 
+    // Remplace la fonction updateCharging
     boolean updateCharging2() {
 
+        // Variable batterie
         float batterie = getShipBatteryLevel();
 
+        // Si la batterie est inférieure à 20
         if (batterie < 20) {
+            // Alors variable charge = vraie
             charge = true;
-        } else if (batterie > 95) {
+            // Sinon si la batterie est supérieure à 90
+        } else if (batterie > 99) {
+            // Alors variable charge = fausse
             charge = false;
         }
 
-        if (charge && batterie < 95) {
+        // Si la charge est activée donc vrai et que la batterie est inférieur à 99
+        if (charge && batterie < 99) {
+            // On retourne vraie
             return true;
+            // Sinon
         } else {
+            // On retourne faux
             return false;
         }
+
     }
 
+    // Fonction pour les mouvements de recharge (Mode batterie)
     float MouvCharge() {
 
         int nbcheckpointmax = getNbRaceCheckPoints(); // Récupération du nombre maximum du checkpoint
@@ -79,6 +94,7 @@ public class Student23 extends PodPlugIn {
 
     }
 
+    // Fonction pour les mouvements de checkpoint (Mode normal)
     float MouvCheck() {
 
         int nextCheckPoint = getNextCheckPointIndex(); // Récupération postion du prochaine checkpoint
@@ -97,6 +113,8 @@ public class Student23 extends PodPlugIn {
 
     }
 
+    // Variable speed
+
     float speed;
 
     // END OF VARIABLES/FUNCTIONS AREA
@@ -107,38 +125,79 @@ public class Student23 extends PodPlugIn {
         //-------------------------------------------------------
         // WRITE YOUR OWN CODE HERE
 
+        // Définir le nom
         setPlayerName("Amandine");
+        // Définir le vaisseau que l'on souhaite
         selectShip(8);
+        // Définir la couleur du vaisseau
         setPlayerColor(43, 59, 236, 255);
+
 
         float DistanceCheck = distanceBetween(getShipPositionX(), getShipPositionY(), getCheckPointX(getNextCheckPointIndex()), getCheckPointY(getNextCheckPointIndex()));
         float DistanceCharge = distanceBetween(getShipPositionX(), getShipPositionY(), getCheckPointX(getFirstCharging()), getCheckPointY(getFirstCharging()));
 
+        // La batterie est activé
         if (updateCharging2() == true) {
+            // alors on execute la fonction turn pour le mouvement de recharge
             turn(MouvCharge());
+            // On set un couleur pour se reperer
             setPlayerColor(255, 255, 255, 255);
-            if (DistanceCharge < 1f) {
-                speed = -0.39f;
-                setPlayerColor(32, 213, 224, 255);
-            } else if (DistanceCharge > 2f) {
+            // Si la vitesse est strictement égale à 0
+            if (speed == 0.0f) {
+                // Si la distance est inférieure à 1
+                if (DistanceCharge < 1f) {
+                    // Alors on mets la vitesse à - 0.39f pour freiner
+                    speed = -0.39f;
+                    setPlayerColor(32, 213, 224, 255);
+                    // Sinon si la distance et supérieur à 2
+                } else if (DistanceCharge > 2f) {
+                    // On mets la vitesse à 0.75 pour accélerer
+                    speed = 0.75f;
+                    setPlayerColor(255, 255, 255, 255);
+                } else {
+                    speed = 0.75f;
+                }
+                // Alors on défini la vitesse à 0.75
                 speed = 0.75f;
-                setPlayerColor(255, 255, 255, 255);
+                // Sinon
             } else {
-                speed = 0.75f;
+                // Si la distance est strictement égale à 0
+                if (DistanceCharge == 0f) {
+                    // On mets le speed -1 pour arreter le vaisseau
+                    speed = -1f;
+                    // Sinon
+                } else {
+                    // On accélère
+                    speed = 0.75f;
+                }
             }
+            // Fonction pour accélerer ou freiner ou on intégre la variable vitesse
             accelerateOrBrake(speed);
         } else {
+            // alors on execute la fonction turn pour le mouvement de checkpoint
             turn(MouvCheck());
-            if (DistanceCheck < 1f) {
-                speed = -0.39f;
-                setPlayerColor(171, 0, 0, 255);
-            } else if (DistanceCheck > 2f) {
+            // Si le speed est strictement égale à 0
+            if (speed == 0.0f) {
+                // Si la distance est inférieure à 1
+                if (DistanceCheck < 1f) {
+                    // Alors on mets la vitesse à - 0.39f pour freiner
+                    speed = -0.39f;
+                    setPlayerColor(171, 0, 0, 255);
+                    // Sinon si la distance et supérieur à 2
+                } else if (DistanceCheck > 2f) {
+                    // On mets la vitesse à 0.75 pour accélerer
+                    speed = 0.75f;
+                    setPlayerColor(253, 0, 0, 255);
+                    // Sinon
+                } else {
+                    // Alors On mets la vitesse à 0.75 par défaut
+                    speed = 0.75f;
+                    setPlayerColor(32, 213, 0, 255);
+                }
+                // On mets la vitesse à 0.75 par défaut
                 speed = 0.75f;
-                setPlayerColor(253, 0, 0, 255);
-            } else {
-                speed = 0.75f;
-                setPlayerColor(32, 213, 0, 255);
             }
+            // Fonction pour accélerer ou freiner ou on intégre la variable vitesse
             accelerateOrBrake(speed);
         }
 
